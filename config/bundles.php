53d96a1104b8e896bb432ca292b91d8d08f36425
Mel Peslier <mel.peslier@gmail.com>
2022-08-04 11:30:31 -0300

    Commit précédent pour celui-ci. Commit précédent : on range mieux les fichiers css

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
