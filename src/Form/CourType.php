1e9f5f927820c027fa8ff548d3151113767d211b
Mel Peslier <mel.peslier@gmail.com>
2022-08-10 09:48:20 -0300

    supression de uses inutiles

--- src/Form/CourType.php
+++ src/Form/CourType.php
@@ -7,9 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CourType extends AbstractType
