64393da3237a5f82ac9a336f0d49d74de75313e2
Mel Peslier <mel.peslier@gmail.com>
2022-08-10 10:47:02 -0300

    parametres sans service .yaml : adresse et nom des mails

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,10 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('info@seasGuyane.com', 'SeasGuyane'))
+                    ->from(new Address(
+                        $this->getParameter('app.mail_from_adress'),
+                        $this->getParameter('app.mail_from_name')
+                    ))
                     ->to($user->getEmail())
                     ->subject('Veuillez confirmer votre adresse mail')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
