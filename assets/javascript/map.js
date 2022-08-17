e9e771c19a1e23f3b0a4dd8a4c65829bdf07549c
Mel Peslier <mel.peslier@gmail.com>
2022-08-17 15:26:03 -0300

    Début de la vue en 'map' du catalogue

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,20 +1,32 @@
+// *****variables*****
 var x_antenne = 4.948832;
 var y_antenne = -52.30964;
+var xy_antenne = [x_antenne, y_antenne];
+// *****fin variables*****
 
-var map = L.map('map').setView([x_antenne, y_antenne], 13);
+
+// *****création map*****
+var map = L.map('map').setView(xy_antenne, 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-var marker = L.marker([x_antenne, y_antenne]).addTo(map);
+map.wrapLatLng;
+// *****fin création map*****
 
-// var circle = L.circle([x_antenne, y_antenne], {
-//     color: 'red',
-//     radius: 500000
-// }).addTo(map);
 
+
+// *****rayon du satellite*****
+var circle = L.circle(xy_antenne, {
+    color: 'red',
+    fill: false,
+    radius: 2500000
+}).addTo(map);
+// *****fin rayon du satellite*****
+
+// *****position on map click*****
 var popup = L.popup();
 
 function onMapClick(e) {
@@ -23,5 +35,17 @@ function onMapClick(e) {
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
 }
-
 map.on('click', onMapClick);
+// *****fin position on map click*****
+
+
+
+// *****antenne****
+var antenneIcon = L.icon({
+    iconUrl: 'images/map/antenne.png',
+    iconSize:     [40, 47], // size of the icon
+    iconAnchor:   [20, 23.5], // point of the icon which will correspond to marker's location
+});
+
+L.marker(xy_antenne, {icon: antenneIcon}).addTo(map);
+// *****fin antenne****
