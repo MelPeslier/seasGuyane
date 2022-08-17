68e7e067e2c6c80bac272867342ac35ea4f56d7e
Mel Peslier <mel.peslier@gmail.com>
2022-08-17 10:29:12 -0300

    incorporation de la map, en test

---
+++ assets/javascript/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([5.232298, -52.803040], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
