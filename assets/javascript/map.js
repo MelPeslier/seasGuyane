769a7a71ec84bff941b1fd12979ae676d79db6a0
Mel Peslier <mel.peslier@gmail.com>
2022-08-17 13:43:14 -0300

    ajouts de base / tests

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,6 +1,27 @@
-var map = L.map('map').setView([5.232298, -52.803040], 13);
+var x_antenne = 4.948832;
+var y_antenne = -52.30964;
+
+var map = L.map('map').setView([x_antenne, y_antenne], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+var marker = L.marker([x_antenne, y_antenne]).addTo(map);
+
+// var circle = L.circle([x_antenne, y_antenne], {
+//     color: 'red',
+//     radius: 500000
+// }).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
